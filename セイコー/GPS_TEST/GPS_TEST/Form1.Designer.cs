6479f17fd7532ad7a7e5ea87d3d0167bcc1e649d
uchitake <yuusuke070920@gmail.com>
2019-01-21 11:06:18 +0900

    No3

---
+++ セイコー/GPS_TEST/GPS_TEST/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GPS_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title_lb = new System.Windows.Forms.Label();
+            this.GPSval_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // title_lb
+            // 
+            this.title_lb.AutoSize = true;
+            this.title_lb.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.title_lb.Location = new System.Drawing.Point(116, 90);
+            this.title_lb.Name = "title_lb";
+            this.title_lb.Size = new System.Drawing.Size(117, 27);
+            this.title_lb.TabIndex = 0;
+            this.title_lb.Text = "GPS情報";
+            // 
+            // GPSval_txt
+            // 
+            this.GPSval_txt.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.GPSval_txt.Location = new System.Drawing.Point(239, 87);
+            this.GPSval_txt.Name = "GPSval_txt";
+            this.GPSval_txt.Size = new System.Drawing.Size(155, 34);
+            this.GPSval_txt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 215);
+            this.Controls.Add(this.GPSval_txt);
+            this.Controls.Add(this.title_lb);
+            this.Name = "Form1";
+            this.Text = "GPSテスト";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title_lb;
+        private System.Windows.Forms.TextBox GPSval_txt;
+    }
+}
+
---
+++ セイコー/Service1/Service1/Service1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace Service1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private int count;
+
+        private Timer timer = new Timer();
+
+        public Service1()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            SetCount();
+        }
+
+        protected override void OnStop()
+        {
+            StopCount();
+        }
+
+        /// <summary>
+        /// 一定処理
+        /// <param name=""></param>
+        /// </summary>
+        private void SetCount()
+        {
+
+            this.timer.Interval = 1000;
+
+            this.timer.Elapsed += new ElapsedEventHandler(OnTimerEvent);
+
+            this.timer.Start();
+
+        }
+
+        /// <summary>
+        /// 停止処理
+        /// <param name=""></param>
+        /// </summary>
+        private void StopCount()
+        {
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// カウント処理
+        /// <param name=""></param>
+        /// </summary>
+        private void OnTimerEvent(object source, ElapsedEventArgs e)
+        {
+            // TODO:
+            Debug.WriteLine("Tick : " + DateTime.Now);
+
+            string sourceName = "MySource";
+
+            byte[] myData = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //ソースが存在していない時は、作成する
+            if (!System.Diagnostics.EventLog.SourceExists(sourceName))
+            {
+                //ログ名を空白にすると、"Application"となる
+                System.Diagnostics.EventLog.CreateEventSource(sourceName, "");
+            }
+
+            System.Diagnostics.EventLog.WriteEntry(
+    sourceName, "Tick : " + DateTime.Now,
+    System.Diagnostics.EventLogEntryType.Error, 1, 1000, myData);
+        }
+    }
+}
