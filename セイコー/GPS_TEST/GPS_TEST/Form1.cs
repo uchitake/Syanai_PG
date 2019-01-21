22949e301aed4bf0eae86edf4d8f7e62ab6fe0fd
uchitake <yuusuke070920@gmail.com>
2019-01-21 09:38:36 +0900

    No1

---
+++ セイコー/GPS_TEST/GPS_TEST/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Device.Location;
+
+namespace GPS_TEST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
+
+            GeoCoordinateWatcher gcw;
+
+            // インスタンスを作成
+            gcw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+
+            gcw.Start();
+
+            GPSval_txt.Text = gcw.Position.Location.Latitude.ToString() + " : " + gcw.Position.Location.Longitude.ToString();
+        }
+    }
+}
